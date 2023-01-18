5c8597c5f2d7ee71cd80eaeb6e9149f9c4f71f38
Joe Osborne <joeosborne77@gmail.com>
2023-01-18 18:19:48 +0000

    Delete backend(api) directory

--- backend(api)/models/authors.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  const authors = sequelize.define("authors", {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
-  return authors;
-};
